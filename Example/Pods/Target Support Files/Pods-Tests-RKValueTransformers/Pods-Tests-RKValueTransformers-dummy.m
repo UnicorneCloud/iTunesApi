138f953e4ba4cdac1b2d7fccec56db0379e8df40
Eric Pinet <pineri01@gmail.com>
2017-12-26 08:58:43 -0500

    Adding support to iOS 11

--- Example/Pods/Target Support Files/Pods-Tests-RKValueTransformers/Pods-Tests-RKValueTransformers-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Tests_RKValueTransformers : NSObject
-@end
-@implementation PodsDummy_Pods_Tests_RKValueTransformers
-@end
--- Example/Pods/Target Support Files/Pods-iTunesApi-RKValueTransformers/Pods-iTunesApi-RKValueTransformers-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iTunesApi_RKValueTransformers : NSObject
-@end
-@implementation PodsDummy_Pods_iTunesApi_RKValueTransformers
-@end
